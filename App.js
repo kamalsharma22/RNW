cbf23286e38c77829aa1092f195ff32ec6b656f9
kamalsharma22 <47597074+kamalsharma22@users.noreply.github.com>
2019-02-15 16:42:57 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import Screen from './src/screen/Screen';
+
+type Props = {};
+const App = (props) => <Screen></Screen>;
+
+export default App
